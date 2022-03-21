04b0a6ae5e33c4c19ad7b13b7f5c5018bed8a549
Hakimuddin Kika <Hakimuddin Kika>
2022-03-21 20:00:10 +0300

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,31 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: {
     content: ['./public/**/*.html'],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      borderWidth:{
+        '10':'10px'
+      },
+      height:{
+        '172':'43rem'
+      },
+      width:{
+        '120':'30rem'
+      },
+      colors:{
+        'fuchsia': colors.fuchsia
+      },
+      fontSize:{
+        'xxs':'0.5rem'
+      },
+      maxWidth:{
+        '32':'8rem'
+      },
+    },
   },
   variants: {
     extend: {},
